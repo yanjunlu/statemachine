1651addd9aa6b460586b8fc9e2861557c2984b2d
luyanjun <13802883173@139.com>
2018-05-29 14:48:05 +0800

    init project

---
+++ src/main/java/com/lu/demo/StatemachineApplication.java
@@ -0,0 +1,12 @@
+package com.lu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatemachineApplication.class, args);
+	}
+}
